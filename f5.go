a178feea95875e4f46147b985a0c6f591d86a556
lxhom <lxhomeis@gmail.com>
2022-02-09 13:00:57 +0100

    languages

---
+++ f5.go
@@ -0,0 +1,104 @@
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)
+}
+func TLSDialer(forward Dialer, config *tlscommon.TLSConfig, timeout time.Duration) Dialer {
+	return TestTLSDialer(testing.NullDriver, forward, config, timeout)}
